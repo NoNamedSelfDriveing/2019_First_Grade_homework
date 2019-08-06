82fd40b59e06cab5c7333be6de906b712f1ec0fb
yujaemin <48317457+youjmen@users.noreply.github.com>
2019-08-06 18:29:03 +0900

    Update LED_Shift_1.c

--- 유재민/1st_week/LED_Shift_1.c
+++ 유재민/1st_week/LED_Shift_1.c
@@ -7,10 +7,10 @@ int main(){
 	
 	int a=0x01,i;
 	while(1){
-		a=0x01;
+		a=0x01;  // a를 0000 0001로 초기화
 		for(i=0;i<8;i++){
-			PORTE=a<<i;
-			_delay_ms(1000);
+			PORTE=a<<i; // a 를 한 칸씩 시프트 하여 0000 0010 0000 0100 ''' 이렇게 다음 LED가 켜지도록 반복한다.
+			_delay_ms(1000); 
 			PORTE=0x00;
 		}
 
