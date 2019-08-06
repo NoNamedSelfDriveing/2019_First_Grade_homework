2b2ac1af694ed73ad296ba0a54be8527cd0df7f2
yujaemin <48317457+youjmen@users.noreply.github.com>
2019-08-06 19:20:35 +0900

    Update LED_Shift_1.c

--- 유재민/1st_week/LED_Shift_1.c
+++ 유재민/1st_week/LED_Shift_1.c
@@ -5,14 +5,18 @@
 int main(){
 	DDRE= 0xFF;
 	
-	int nPin=0x01,i;
+	int nPin=0x01,i=0; // nPin를 0000 0001로 초기화
 	while(1){
-		nPin=0x01;  // nPin를 0000 0001로 초기화
-		for(i=0;i<8;i++){
-			PORTE=nPin<<i; // nPin  한 칸씩 시프트 하여 0000 0010 0000 0100 ''' 이렇게 다음 LED가 켜지도록 반복한다.
-			_delay_ms(1000); 
-			PORTE=0x00;
-		}
+	if(i%8==0){
+	    nPin=0x01; //8번째 LED가 켜지면 nPin과 i의 값을 다시 초기화
+	    i=0;
+	}
+	
+		
+		PORTE=nPin<<i; // nPin  한 칸씩 시프트 하여 0000 0010 0000 0100 ''' 이렇게 다음 LED가 켜지도록 반복한다.
+		_delay_ms(1000); 
+		PORTE=0x00;
+		
 
 	}
 	return 0;
